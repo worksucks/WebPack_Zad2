23d2dd3667e144fd481431410e82bf83e1c2bcfe
Tomasz Reizer <t_reizer@yahoo.com>
2019-01-26 05:34:45 +0100

    Added configuration files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+//webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                  use: [
+                      { loader: 'style-loader'},
+                      {
+                          loader: 'css-loader',
+                          options: {
+                            modules: true
+                          }
+                      }
+                ]
+            }
+        ]
+    }
+};
